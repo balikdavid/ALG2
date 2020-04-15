7b9b436b37cdd68e28cd904935e254fb92471e94
balikdavid <david.balik@tul.cz>
2020-04-15 13:42:36 +0200

    Student

---
+++ Student/Main.java
@@ -0,0 +1,15 @@
+
+package cvika5_video3_comparing_interface;
+
+/**
+ *
+ * @author David Bálik
+ */
+public class Main {
+
+  
+    public static void main(String[] args) {
+       
+    }
+    
+}
